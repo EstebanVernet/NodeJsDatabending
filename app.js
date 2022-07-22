add03e7330909a7f702400b004a8ffcfa328e08e
Esteban Vernet <contact@estebanver.net>
2022-07-22 11:14:19 +0200

    Added installation and run files

--- app.js
+++ app.js
@@ -123,7 +123,5 @@ app.post('/imgprocess', function(req,res){
 });
 
 var server = app.listen(8000, function () {
-  var host = server.address().address
-  var port = server.address().port
-  console.log("Example app listening at http://%s:%s", host, port)
+  console.log("The application is running...")
 })
